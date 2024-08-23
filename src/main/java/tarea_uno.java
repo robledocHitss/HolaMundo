3d4346732042b2fd8215179923c44cca87509b75
robledocHitss <robledoc@globalhitss.com>
2024-08-22 18:12:50 -0600

    Update tarea_uno.java

--- src/main/java/tarea_uno.java
+++ src/main/java/tarea_uno.java
@@ -1,5 +1,5 @@
 public class tarea_uno {
     public static void main(String[] args) {
-        System.out.println("Hola Mundo");
+        System.out.println("Hola Mundo, Hola Global Hitss");
     }
 }
